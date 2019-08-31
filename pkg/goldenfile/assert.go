d0aa501b69152aaad6933dae7c2f3b4c356317d8
Steve Moyer <smoyer1@selesy.com>
2019-08-30 21:25:08 -0400

    Changes parameter name to filepath

--- pkg/goldenfile/assert.go
+++ pkg/goldenfile/assert.go
@@ -4,30 +4,30 @@ package goldenfile
 // Assertions and requirements (in the same fashion as Testify)
 //
 
-func assertEqual(t TestingT, eq GoldenFileComparisonAssertionFunc, goldenFile string, actual []byte, msgAndArgs ...interface{}) bool {
+func assertEqual(t TestingT, eq GoldenFileComparisonAssertionFunc, filepath string, actual []byte, msgAndArgs ...interface{}) bool {
 	if h, ok := t.(tHelper); ok {
 		h.Helper()
 	}
 	if *Update {
-		writeGoldenFile(t, goldenFile, actual)
+		writeGoldenFile(t, filepath, actual)
 		return true
 	}
-	expected := readGoldenFile(t, goldenFile)
+	expected := readGoldenFile(t, filepath)
 	return eq(t, expected, actual, msgAndArgs...)
 }
 
-func AssertBytesEq(t TestingT, goldenFile string, actual []byte, msgAndArgs ...interface{}) bool {
-	return assertEqual(t, bytesEq, goldenFile, actual, msgAndArgs...)
+func AssertBytesEq(t TestingT, filepath string, actual []byte, msgAndArgs ...interface{}) bool {
+	return assertEqual(t, bytesEq, filepath, actual, msgAndArgs...)
 }
 
-func AssertJSONEq(t TestingT, goldenFile string, actual string, msgAndArgs ...interface{}) bool {
-	return assertEqual(t, jsonEq, goldenFile, []byte(actual), msgAndArgs...)
+func AssertJSONEq(t TestingT, filepath string, actual string, msgAndArgs ...interface{}) bool {
+	return assertEqual(t, jsonEq, filepath, []byte(actual), msgAndArgs...)
 }
 
-func AssertStringEq(t TestingT, goldenFile string, actual string, msgAndArgs ...interface{}) bool {
-	return assertEqual(t, stringEq, goldenFile, []byte(actual), msgAndArgs...)
+func AssertStringEq(t TestingT, filepath string, actual string, msgAndArgs ...interface{}) bool {
+	return assertEqual(t, stringEq, filepath, []byte(actual), msgAndArgs...)
 }
 
-func AssertYAMLEq(t TestingT, goldenFile string, actual string, msgAndArgs ...interface{}) bool {
-	return assertEqual(t, yamlEq, goldenFile, []byte(actual), msgAndArgs...)
+func AssertYAMLEq(t TestingT, filepath string, actual string, msgAndArgs ...interface{}) bool {
+	return assertEqual(t, yamlEq, filepath, []byte(actual), msgAndArgs...)
 }
--- pkg/goldenfile/require.go
+++ pkg/goldenfile/require.go
@@ -4,8 +4,8 @@ package goldenfile
 // Requirements (in the same fashion as Testify)
 //
 
-func requireEqual(t TestingT, eq GoldenFileComparisonAssertionFunc, goldenFile string, actual []byte, msgAndArgs ...interface{}) {
-	if assertEqual(t, eq, goldenFile, actual, msgAndArgs...) {
+func requireEqual(t TestingT, eq GoldenFileComparisonAssertionFunc, filepath string, actual []byte, msgAndArgs ...interface{}) {
+	if assertEqual(t, eq, filepath, actual, msgAndArgs...) {
 		return
 	}
 	if h, ok := t.(tHelper); ok {
@@ -14,18 +14,18 @@ func requireEqual(t TestingT, eq GoldenFileComparisonAssertionFunc, goldenFile s
 	t.FailNow()
 }
 
-func RequireBytesEq(t TestingT, goldenFile string, actual []byte, msgAndArgs ...interface{}) {
-	requireEqual(t, bytesEq, goldenFile, actual, msgAndArgs...)
+func RequireBytesEq(t TestingT, filepath string, actual []byte, msgAndArgs ...interface{}) {
+	requireEqual(t, bytesEq, filepath, actual, msgAndArgs...)
 }
 
-func RequireJSONEq(t TestingT, goldenFile string, actual string, msgAndArgs ...interface{}) {
-	requireEqual(t, jsonEq, goldenFile, []byte(actual), msgAndArgs...)
+func RequireJSONEq(t TestingT, filepath string, actual string, msgAndArgs ...interface{}) {
+	requireEqual(t, jsonEq, filepath, []byte(actual), msgAndArgs...)
 }
 
-func RequireStringEq(t TestingT, goldenFile string, actual string, msgAndArgs ...interface{}) {
-	requireEqual(t, stringEq, goldenFile, []byte(actual), msgAndArgs...)
+func RequireStringEq(t TestingT, filepath string, actual string, msgAndArgs ...interface{}) {
+	requireEqual(t, stringEq, filepath, []byte(actual), msgAndArgs...)
 }
 
-func RequireYAMLEq(t TestingT, goldenFile string, actual string, msgAndArgs ...interface{}) {
-	requireEqual(t, yamlEq, goldenFile, []byte(actual), msgAndArgs...)
+func RequireYAMLEq(t TestingT, filepath string, actual string, msgAndArgs ...interface{}) {
+	requireEqual(t, yamlEq, filepath, []byte(actual), msgAndArgs...)
 }
