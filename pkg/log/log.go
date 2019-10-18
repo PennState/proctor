e65541cf951ba9a11d0e822811e98a8933291e17
Steve Moyer <smoyer@psu.edu>
2019-10-18 16:23:06 -0400

    Wraps logrus Log methods as test helpers
    Resolves #15

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -4,22 +4,6 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// Logger returns a new logrus Logger that outputs its data to the
-// testing package's Log() method.
-func New(t TestingT) *logrus.Logger {
-	if h, ok := t.(tHelper); ok {
-		h.Helper()
-	}
-	l, err := logrus.ParseLevel(*level)
-	if err != nil {
-		t.Log(err)
-	}
-	logger := logrus.New()
-	logger.SetLevel(l)
-	logger.SetOutput(logWriter{t: t})
-	return logger
-}
-
 type logWriter struct {
 	t TestingT
 }
@@ -30,3 +14,97 @@ func (w logWriter) Write(p []byte) (int, error) {
 	w.t.Log(string(p))
 	return len(p), nil
 }
+
+// Logger wraps the logrus.Logger methods as test helpers
+type Logger struct {
+	logger *logrus.Logger
+	Debug  log  // Debug logs the provided arguments at debug level
+	Debugf logf // Debugf logs the format produced string at debug level
+	Error  log
+	Errorf logf
+	Fatal  log
+	Fatalf logf
+	Info   log
+	Infof  logf
+	Panic  log
+	Panicf logf
+	Trace  log
+	Tracef logf
+	Warn   log
+	Warnf  logf
+}
+
+// Logger returns a new logrus Logger that outputs its data to the
+// testing package's Log() method.
+func New(t TestingT) *Logger {
+	if h, ok := t.(tHelper); ok {
+		h.Helper()
+	}
+
+	logger := logrus.New()
+	return Wrap(t, logger)
+}
+
+func Wrap(t TestingT, logger *logrus.Logger) *Logger {
+	if h, ok := t.(tHelper); ok {
+		h.Helper()
+	}
+
+	l, err := logrus.ParseLevel(*level)
+	if err != nil {
+		t.Log(err)
+	}
+	logger.SetLevel(l)
+	logger.SetOutput(logWriter{t: t})
+	return &Logger{
+		logger: logger,
+		Debug:  wrap(t, logger, logrus.DebugLevel),
+		Debugf: wrapf(t, logger, logrus.DebugLevel),
+		Error:  wrap(t, logger, logrus.ErrorLevel),
+		Errorf: wrapf(t, logger, logrus.ErrorLevel),
+		Fatal:  wrap(t, logger, logrus.FatalLevel),
+		Fatalf: wrapf(t, logger, logrus.FatalLevel),
+		Info:   wrap(t, logger, logrus.InfoLevel),
+		Infof:  wrapf(t, logger, logrus.InfoLevel),
+		Panic:  wrap(t, logger, logrus.PanicLevel),
+		Panicf: wrapf(t, logger, logrus.PanicLevel),
+		Trace:  wrap(t, logger, logrus.TraceLevel),
+		Tracef: wrapf(t, logger, logrus.TraceLevel),
+		Warn:   wrap(t, logger, logrus.WarnLevel),
+		Warnf:  wrapf(t, logger, logrus.WarnLevel),
+	}
+}
+
+type log func(...interface{})
+type logf func(format string, args ...interface{})
+
+func wrap(t TestingT, logger *logrus.Logger, level logrus.Level) log {
+	return func(args ...interface{}) {
+		if h, ok := t.(tHelper); ok {
+			h.Helper()
+		}
+
+		logger.Log(level, args)
+	}
+}
+
+func wrapf(t TestingT, logger *logrus.Logger, level logrus.Level) logf {
+	return func(format string, args ...interface{}) {
+		if h, ok := t.(tHelper); ok {
+			h.Helper()
+		}
+
+		logger.Logf(level, format, args)
+	}
+}
+
+// var (
+// 	Debug reporter = f
+// )
+
+// func Debug(args interface{}) {
+// 	if h, ok := t.(tHelper); ok {
+// 		h.Helper()
+// 	}
+
+// }
